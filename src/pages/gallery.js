614cd0832f718b5c44fbbe9bac2b67468b722874
dishabhoje <103805351+dishabhoje@users.noreply.github.com>
2023-02-07 23:31:38 +0530

    done with basic website

---
+++ src/pages/gallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Frame from '../components/frames'
+
+const GalleryPage = () => {
+  return (
+    <Frame>
+      <p>This will be passed in as children</p>
+    </Frame>
+  )
+}
+
+export default GalleryPage
